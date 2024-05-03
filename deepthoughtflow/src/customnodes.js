c46767a9f06868c9e4f493a9504f639dcd233bec
mark <mark@kirosoft.co.uk>
2024-05-03 22:58:34 +0100

    some extra types added

--- deepthoughtflow/src/customnodes.js
+++ deepthoughtflow/src/customnodes.js
@@ -10,7 +10,9 @@ function AgentNode(){
 	}
 	var that = this;
 	this.temperature = this.addWidget("slider","Temperature", 0.5, function(v){}, { min: 0, max: 1} );
-	this.role = this.addWidget("combo","role", "default", function(v){}, { values:["ukho","lawyer","researcher"]} );
+	this.role = this.addWidget("combo","role", "default", function(v){
+		that.setProperty("role",v);
+	}, { values:["ukho","lawyer","researcher"]} );
 	this.text = this.addWidget("text","Examples", "multiline", function(v){}, { multiline:true } );
 
 	this.addInput("Input Request","text");
@@ -46,6 +48,133 @@ function TestWidgetsNode()
 TestWidgetsNode.title = "Widgets";
 
 LiteGraph.registerNodeType("features/widgets", TestWidgetsNode );
+
+//Constant
+function Time() {
+	this.addOutput("in ms", "number");
+	this.addOutput("in sec", "number");
+}
+
+Time.title = "Time";
+Time.desc = "Time";
+
+Time.prototype.onExecute = function() {
+	this.setOutputData(0, this.graph.globaltime * 1000);
+	this.setOutputData(1, this.graph.globaltime);
+};
+
+LiteGraph.registerNodeType("basic/time", Time);
+
+//to store json objects
+function JSONParse() {
+	this.addInput("parse", LiteGraph.ACTION);
+	this.addInput("json", "string");
+	this.addOutput("done", LiteGraph.EVENT);
+	this.addOutput("object", "object");
+	this.widget = this.addWidget("button","parse","",this.parse.bind(this));
+	this._str = null;
+	this._obj = null;
+}
+
+JSONParse.title = "JSON Parse";
+JSONParse.desc = "Parses JSON String into object";
+
+JSONParse.prototype.parse = function()
+{
+	if(!this._str)
+		return;
+
+	try {
+		this._str = this.getInputData(1);
+		this._obj = JSON.parse(this._str);
+		this.boxcolor = "#AEA";
+		this.triggerSlot(0);
+	} catch (err) {
+		this.boxcolor = "red";
+	}
+}
+
+JSONParse.prototype.onExecute = function() {
+	this._str = this.getInputData(1);
+	this.setOutputData(1, this._obj);
+};
+
+JSONParse.prototype.onAction = function(name) {
+	if(name == "parse")
+		this.parse();
+}
+
+LiteGraph.registerNodeType("basic/jsonparse", JSONParse);	
+
+//Show value inside the debug console
+function TimerEvent() {
+	this.addProperty("interval", 1000);
+	this.addProperty("event", "tick");
+	this.addOutput("on_tick", LiteGraph.EVENT);
+	this.time = 0;
+	this.last_interval = 1000;
+	this.triggered = false;
+}
+
+TimerEvent.title = "Timer";
+TimerEvent.desc = "Sends an event every N milliseconds";
+
+TimerEvent.prototype.onStart = function() {
+	this.time = 0;
+};
+
+TimerEvent.prototype.getTitle = function() {
+	return "Timer: " + this.last_interval.toString() + "ms";
+};
+
+TimerEvent.on_color = "#AAA";
+TimerEvent.off_color = "#222";
+
+TimerEvent.prototype.onDrawBackground = function() {
+	this.boxcolor = this.triggered
+		? TimerEvent.on_color
+		: TimerEvent.off_color;
+	this.triggered = false;
+};
+
+TimerEvent.prototype.onExecute = function() {
+	var dt = this.graph.elapsed_time * 1000; //in ms
+
+	var trigger = this.time == 0;
+
+	this.time += dt;
+	this.last_interval = Math.max(
+		1,
+		this.getInputOrProperty("interval") | 0
+	);
+
+	if (
+		!trigger &&
+		(this.time < this.last_interval || isNaN(this.last_interval))
+	) {
+		if (this.inputs && this.inputs.length > 1 && this.inputs[1]) {
+			this.setOutputData(1, false);
+		}
+		return;
+	}
+
+	this.triggered = true;
+	this.time = this.time % this.last_interval;
+	this.trigger("on_tick", this.properties.event);
+	if (this.inputs && this.inputs.length > 1 && this.inputs[1]) {
+		this.setOutputData(1, true);
+	}
+};
+
+TimerEvent.prototype.onGetInputs = function() {
+	return [["interval", "number"]];
+};
+
+TimerEvent.prototype.onGetOutputs = function() {
+	return [["tick", "boolean"]];
+};
+
+LiteGraph.registerNodeType("events/timer", TimerEvent);
 
 
 
